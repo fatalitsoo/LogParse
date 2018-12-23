b1a6b311baf52744c65e855c2235397ed7b6e616
Ритфаль Галиуллин <fatal2308@mail.ru>
2018-12-23 22:09:01 +0500

    first

---
+++ LogParser/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace LogParser
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(25, 275);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(673, 163);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(25, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(159, 59);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "загрузить логи и создать базу";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(531, 155);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "1. Ответ";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(135, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "D:\\logs1.txt";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(7, 10);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(122, 20);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Путь до логов:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(274, 12);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(201, 20);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Путь для создания базы:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(481, 12);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 6;
+			this.textBox2.Text = "D:\\LogBase\\Logs";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(286, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(189, 16);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "(название без расширения)";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(243, 63);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(0, 16);
+			this.label4.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(8, 100);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(221, 16);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "(может занять некоторое время)";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.label6.Location = new System.Drawing.Point(8, 158);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(506, 16);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "1.\tПосетители из какой страны совершают больше всего действий на сайте?";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.label7.Location = new System.Drawing.Point(8, 188);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(641, 16);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "2.\tПосетители из какой страны чаще всего интересуются товарами из определенных ка" +
+    "тегорий?";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(846, 185);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 13;
+			this.button2.Text = "1. Ответ";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "fresh_fish",
+            "canned_food",
+            "semi_manufactures",
+            "caviar",
+            "frozen_fish"});
+			this.comboBox1.Location = new System.Drawing.Point(673, 188);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 14;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "fresh_fish",
+            "canned_food",
+            "semi_manufactures",
+            "caviar",
+            "frozen_fish"});
+			this.comboBox2.Location = new System.Drawing.Point(675, 220);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 17;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(848, 217);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 16;
+			this.button4.Text = "1. Ответ";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.label8.Location = new System.Drawing.Point(10, 220);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(576, 16);
+			this.label8.TabIndex = 15;
+			this.label8.Text = "3. В какое время суток чаще всего просматривают определенную категорию товаров?";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(933, 450);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label8;
+	}
+}
+
---
+++ LogParser/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using MaxMind.Db;
+using MaxMind.GeoIP2;
+
+namespace LogParser
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		DataSet ds = new DataSet();
+		Random rnd = new Random();
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string Костыль = "Log" + rnd.Next(1, 1000000).ToString();
+			if (textBox1.Text == "" && textBox2.Text == "")
+				MessageBox.Show("Путь пуст:(");
+			else
+			{
+				StreamReader sr = new StreamReader(textBox1.Text);
+				ds = new DataSet();
+				ds.Tables.Add("Logs");
+				ds.Tables[0].Columns.Add("1");
+				ds.Tables[0].Columns.Add("2");
+				ds.Tables[0].Columns.Add("3");
+				ds.Tables[0].Columns.Add("4");
+				ds.Tables[0].Columns.Add("5");
+				ds.Tables[0].Columns.Add("6");
+				ds.Tables[0].Columns.Add("7");
+				ds.Tables[0].Columns.Add("Dates");
+				ds.Tables[0].Columns.Add("Times");
+				ds.Tables[0].Columns.Add("Keys");
+				ds.Tables[0].Columns.Add("11");
+				ds.Tables[0].Columns.Add("IP");
+				ds.Tables[0].Columns.Add("URL");
+				ds.Tables[0].Columns.Add("Country");
+				string row1 = sr.ReadLine();
+				while (row1 != null)
+				{
+					string[] val = System.Text.RegularExpressions.Regex.Split(row1, " ");
+					ds.Tables[0].Rows.Add(val);
+					row1 = sr.ReadLine();
+				}
+				dataGridView1.DataSource = ds.Tables[0];
+				for (int i = 1; i < 8; i++)
+					dataGridView1.Columns.Remove(i.ToString());
+				dataGridView1.Columns.Remove("11");
+				using (var reader = new DatabaseReader("GeoLite2-Country.mmdb"))
+				{
+					foreach (DataGridViewRow row in dataGridView1.Rows)
+					{
+						try
+						{
+							string a = row.Cells[3].Value.ToString();
+							var city = reader.Country(a);
+							row.Cells[5].Value = city.Country.Name;
+						}
+						catch
+						{
+							row.Cells[5].Value = "Unknown country";
+						}
+					}
+				}
+				String str;
+				SqlConnection myConn = new SqlConnection(" Data Source = (LocalDB)\\MSSQLLocalDB;Integrated Security = True;  Connect Timeout = 30;database=master");
+				str = "CREATE DATABASE " + Костыль + " ON PRIMARY " +
+					"(NAME = MyDatabase_Data, " +
+					"FILENAME ='" + textBox2.Text + ".mdf', " +
+					"SIZE = 3MB, MAXSIZE = 10MB, FILEGROWTH = 10%) " +
+					"LOG ON (NAME = MyDatabase_Log, " +
+					"FILENAME ='" + textBox2.Text + ".ldf', " +
+					"SIZE = 1MB, " +
+					"MAXSIZE = 5MB, " +
+					"FILEGROWTH = 10%)";
+				SqlCommand myCommand = new SqlCommand(str, myConn);
+				try
+				{
+					myConn.Open();
+					myCommand.ExecuteNonQuery();
+				}
+				catch
+				{ 
+					System.IO.File.Delete(@textBox2.Text + ".mdf");
+					System.IO.File.Delete(@textBox2.Text + ".ldf");
+				}
+				finally
+				{
+					if (myConn.State == ConnectionState.Open)
+					{
+						myConn.Close();
+					}
+				}
+				str = "create table Logs(Dates Date not null, Times TIME not null, Keys varchar(max) not null, IP varchar(max) not null, URL varchar(max) not null , Country varchar(max))";
+				SqlConnection myConn1 = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + textBox2.Text + ".mdf;Integrated Security=True;Connect Timeout=30");
+				SqlCommand myCommand1 = new SqlCommand(str, myConn1);
+				str = "INSERT INTO Logs (Dates, Times, Keys, IP, URL,Country) VALUES(@Dates, @Times, @Keys, @IP,@URL,@Country)";
+				SqlCommand sc = new SqlCommand(str, myConn1);
+				try
+				{
+					myConn1.Open();
+					myCommand1.ExecuteNonQuery();
+					foreach (DataRow row in ds.Tables[0].Rows)
+					{
+						sc.Parameters.Clear();
+						sc.Parameters.AddWithValue("@Dates", row["Dates"]);
+						sc.Parameters.AddWithValue("@Times", row["Times"]);
+						sc.Parameters.AddWithValue("@Keys", row["Keys"]);
+						sc.Parameters.AddWithValue("@IP", row["IP"]);
+						sc.Parameters.AddWithValue("@URL", row["URL"]);
+						sc.Parameters.AddWithValue("@Country", row["Country"]);
+						sc.ExecuteNonQuery();
+					}
+					myConn1.Close();
+					MessageBox.Show("База MS SQL создана");
+				}
+				catch { MessageBox.Show("Таблица не создана"); }
+			}
+		}
+
+
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + textBox2.Text + ".mdf;Integrated Security=True;Connect Timeout=30";
+			using (SqlConnection connection = new SqlConnection(conString))
+			{
+				connection.Open();
+				using (SqlCommand command = new SqlCommand(
+					"Select top 1 Country, count(*) as Counts from Logs group by Country order by Counts desc;",
+					connection))
+				{
+					using (SqlDataReader reader = command.ExecuteReader())
+					{
+						while (reader.Read())
+						{
+							MessageBox.Show("Посетители из "+reader.GetString(0)+" совершают больше всего действий: "+(reader.GetInt32(1)).ToString());
+						}
+					}
+				}
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if(comboBox1.SelectedItem!=null)
+			{ 
+				string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + textBox2.Text + ".mdf;Integrated Security=True;Connect Timeout=30";
+				using (SqlConnection connection = new SqlConnection(conString))
+				{
+					connection.Open();
+					using (SqlCommand command = new SqlCommand(
+						"Select top 1 Country, count(*) as Counts from Logs where URL like '%"+comboBox1.SelectedItem.ToString()+"%' group by Country order by Counts desc;",
+						connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								MessageBox.Show("Посетители из " + reader.GetString(0) + " чаще всего интересуются товарами из: " + comboBox1.SelectedItem.ToString());
+							}
+						}
+					}
+				}
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			if (comboBox2.SelectedItem != null)
+			{
+				string conString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + textBox2.Text + ".mdf;Integrated Security=True;Connect Timeout=30";
+				using (SqlConnection connection = new SqlConnection(conString))
+				{
+					connection.Open();
+					using (SqlCommand command = new SqlCommand(
+						"select * from (select count(*) as AM from Logs where Times between '00:00:00' and '11:59:59' and URL like '%" + comboBox2.SelectedItem.ToString() + "%') as AM,(select count(*) as PM from Logs where Times between '12:00:00' and '23:59:59' and URL like  '%" + comboBox2.SelectedItem.ToString() + "%') as PM ",
+						connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+							if(reader.GetInt32(0)> reader.GetInt32(1))
+								MessageBox.Show("Посетители чаще просматриваю товары категории: "+ comboBox2.SelectedItem.ToString() + " до полудня " + reader.GetInt32(0) + " vs " + reader.GetInt32(1));
+							else
+									MessageBox.Show("Посетители чаще просматриваю товары категории: " + comboBox2.SelectedItem.ToString() + " до после полудня " + reader.GetInt32(0)+" vs "+ reader.GetInt32(1));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
